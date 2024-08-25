5f7ca1b4478f0e8ad4448c209922f3c84407968d
restodruid1 <106450663+restodruid1@users.noreply.github.com>
2024-08-24 22:41:37 -0700

    Initial commit

---
+++ testAPI.py
@@ -0,0 +1,71 @@
+import requests
+import json
+
+
+
+loop = True
+#file = open("Personal_MTG_Database.txt", "a")
+while loop:
+	cardName = input("Enter Card Name: ")
+	#url = "https://api.scryfall.com/cards/named?fuzzy=aust+com&format=image"  #If want image
+	url = "https://api.scryfall.com/cards/autocomplete?q={}".format(cardName)
+
+	headers = {
+		"User-Agent" : "testAPI.py/1.0"
+		#"Accept" : "*/*"
+	}
+	response = requests.get(url,headers)
+
+	if response.status_code == 200:
+		try:
+			# Turn response into dictionary
+			jsonStr = response.content.decode('utf-8')
+			data = json.loads(jsonStr)
+			print("Cards with {}:".format(cardName))
+			for number, card in enumerate(data["data"]):
+				print("{}) {}".format(number, card))
+			
+			num = int(input("Enter the number for the card you want: "))
+			chosenCard = ""
+			for number, card in enumerate(data["data"], start=1):
+				if number == num:
+					print("yes", card)
+					chosenCard = card
+					break
+			print(chosenCard)
+			
+			with open("MTG_Database.txt", "a+") as file:
+				
+				
+				file.write("{}, 0\n".format(chosenCard))
+				try:
+					file.seek(0)
+					for line in file:
+						print(line)
+						line = line.split(",")
+						print(line)
+						word = line[0]
+						count = int(line[1])
+						print(word, count)
+						#line = list(line)
+						#word = line[0:-1]
+						#count = line[-1]
+						
+				except:
+					print("File is empty")
+					file.write("placeholder", 0)
+					#for line in file:
+						#print(line)
+						#if line[0] == chosenCard:
+							#line[1] += 1
+							#break
+					#file.write(chosenCard, 1)	
+	    		
+			continue	
+		except:
+			print("Error? ")
+		else:
+			print("Error? No card matches")
+	    
+	else:
+	    print(f"Error: {response.status_code}")
