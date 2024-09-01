f3695a37af7911daca15c46ad734ac0c3b4d0240
restodruid1 <106450663+restodruid1@users.noreply.github.com>
2024-09-01 11:50:49 -0700

    Add files via upload

--- pythonClient.py
+++ pythonClient.py
@@ -5,8 +5,8 @@ def start_client():
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     # Get local machine name
-    host = '' #socket.gethostname()
-    port = 8084  # Server port
+    host = '192.168.4.37' #socket.gethostname()
+    port = 8081  # Server port
 
     try:
         # Connection to hostname on the port.
@@ -15,16 +15,27 @@ def start_client():
         # Send a message to the server
         #message = "Hello, Server!"
         while True:
-            message = input("Search for a card: ")
-
+            message = ""
+            action = int(input("Search or Delete Card? Enter 1 for search or 2 for delete or 3 to quit: "))
+            if action == 1:
+                message = "1"
+                message += input("Search for a card: ")
+            elif action == 2:
+                message = "2"
+                message += input("Delete a card: ")
+            elif action == 3:
+                message = "3q"
+            else:
+                continue
             client_socket.send(message.encode('utf-8'))
 
             # Receive response from the server
             response = client_socket.recv(1024).decode('utf-8')
             print(f"Received response: {response}")
-            if message == 'q':
+            if action == 3:
                 break
-
+    except:
+        print("error")
     finally:
         # Close the connection
         client_socket.close()
